8935a5ceb05d43e3746477f4f39a174db8e03b18
Joao Jesus <eng.joaojeus@gmail.com>
2021-09-15 13:42:08 -0300

    pagina de cadastro com localstorage ok sem comentario

--- src/paginas/cadastro/cadastro.js
+++ src/paginas/cadastro/cadastro.js
@@ -1,17 +1,5 @@
 import {useState} from 'react';
 
-
-// var atual = 600000.00;
-
-// //com R$
-// var f = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-
-// //sem R$
-// var f2 = atual.toLocaleString('pt-br', {minimumFractionDigits: 2});
-
-// console.log(f);
-// console.log(f2);
-
 function cadastrar(nome, valor) {  
   let inventario = []  
   if(localStorage.getItem('inventario')) {
